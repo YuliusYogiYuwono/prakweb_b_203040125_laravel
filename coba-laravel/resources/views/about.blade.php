265ec0c268c0ff15c5309dbba43a44c395e1cb8c
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-10-30 17:41:33 +0700

    Update about.blade.php

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -8,5 +8,5 @@
 <h3>{{ $name }}</h3>
 <p>{{ $email }}</p>
 
-<img src="img/{{ $image; }}" alt="{{ $name; }}" width="200" class="img-thumbnail rounded-circle">
+<img src="img/{{ $image; }}" alt="{{ $name; }}" width="200">
 @endsection
