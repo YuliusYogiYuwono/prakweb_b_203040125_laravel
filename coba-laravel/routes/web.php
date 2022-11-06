5b1c24b8ccad154d5fc6b91af1bcdae4a9e2df34
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-11-06 22:46:20 +0700

    menyelesaikan video 13
    menyelesaikan video 13

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -22,20 +22,21 @@ use App\Models\User;
 Route::get('/', function () {
     return view('home', [
         "title" => "Home",
-        "active" => 'posts'
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        'active' => 'about',
         "name" => "Yulius Yogi Yuwono",
         "email" => "yogi@gmail.com",
         "image" => "yogi.jpeg"
     ]);
 });
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function () {
@@ -46,23 +47,23 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view(
-        'posts',
-        [
-            'title' => "Post By Category : $category->name",
-            'active' => 'categories',
-            'posts' => $category->posts->load('category', 'author'),
-        ]
-    );
-});
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     return view(
+//         'posts',
+//         [
+//             'title' => "Post By Category : $category->name",
+//             'active' => 'categories',
+//             'posts' => $category->posts->load('category', 'author'),
+//         ]
+//     );
+// });
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view(
-        'posts',
-        [
-            'title' => "Post by Author : $author->name",
-            'posts' => $author->posts->load('category', 'author'),
-        ]
-    );
-});
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view(
+//         'posts',
+//         [
+//             'title' => "Post by Author : $author->name",
+//             'posts' => $author->posts->load('category', 'author'),
+//         ]
+//     );
+// });
