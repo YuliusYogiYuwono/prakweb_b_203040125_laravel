cb2c1a4b58365b1d5aec757f9a73e75034fd51f6
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-11-06 22:38:58 +0700

    menyelesaikan video 13
    menyelesaikan video 13

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,7 +4,7 @@
 <article>
   <h2>{{ $post->title }}</h2>
 
-  <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+  <p>By. <a href="posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
     {{ $post->category->name }}</a></p>
 
   <!-- <h5>{{ $post["author"] }}</h5> -->
