2f26f9fc108cf500890c50cbc007b5154c19f3fc
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-11-06 22:27:57 +0700

    menyelesaikan video 13
    menyelesaikan video 13

--- coba-laravel/app/Http/Controllers/PostController.php
+++ coba-laravel/app/Http/Controllers/PostController.php
@@ -2,22 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
   public function index()
   {
+    $title = '';
+    if (request('category')) {
+      $category = Category::firstWhere('slug', request('category'));
+      $title = ' in ' . $category->name;
+    }
+
+    if (request('author')) {
+      $author = User::firstWhere('username', request('author'));
+      $title = ' by ' . $author->name;
+    }
+    
     return view('posts', [
-      "title" => "All Posts",
+      "title" => "All Posts" . $title,
       "active" => 'posts',
       // "posts" => Post::all()
-      "posts" => Post::with(['author', 'category'])->latest()->get()
+      "posts" => Post::latest()->filter(request(['search', 'category', 'author']))
+        ->paginate(5)->withQueryString()
     ]);
   }
 
-  public function show(post $post)
+  public function show(Post $post)
   {
 
     return view('post', [
