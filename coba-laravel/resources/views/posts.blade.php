6e45f13f3e232c7ef77d7b118338bc6ae421b61c
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-11-06 22:44:11 +0700

    menyelesaikan video 13
    menyelesaikan video 13

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row justify-content-center mb-3">
+  <div class="col-md-6">
+    <form action="/posts">
+      @if (request('category'))
+      <input type="hidden" name="category" value="{{ request('category') }}">
+      @endif
+      @if (request('author'))
+      <input type="hidden" name="author" value="{{ request('author') }}">
+      @endif
+
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+        <button class="btn btn-danger" type="submit">Search</button>
+      </div>
+    </form>
+  </div>
+</div>
 
 @foreach ($posts as $post)
 <article class="mb-5 border-bottom pb-3">
@@ -9,7 +27,7 @@
     <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
   </h2>
 
-  <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+  <p>By. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">
     {{ $post->category->name }}</a></p>
 
   <p>{{ $post->excerpt }}</p>
