bad6a6335f18a9a4198e203976a110d59bf7b9fe
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-12-01 15:35:29 +0700

    menyelesaikan video 19. Validation & Insert Post
    menyelesaikan video 19. Validation & Insert Post

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -7,17 +7,28 @@
       <h1 class="mb-3">{{ $posts->title }}</h1>
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
-      <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-      <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete </a>
+      <a href="/dashboard/posts/{{ $posts->slug}}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+      <form action="/dashboard/posts/{{ $posts->slug }}" method="post" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span>Delete</button>
+      </form>
+
+      @if($posts->image)
+      <div style="max-height: 350px; overflow:hidden;">
+        <img src="{{ asset('storage/' . $posts->image) }}" alt="{{ $posts->category->name }}" class="img-fluid mt-3">
+      </div>
+      @else
 
       <img src="https://source.unsplash.com/1200x400?{{ $posts->category->name }}" alt="{{ $posts->category->name }}" class="img-fluid mt-3">
+      @endif
 
       <article class="my-3 fs-5">
         {!! $posts->body !!}
       </article>
 
-      <a href="/posts" class="d-block mt-3">Back to Post</a>
 
+      
     </div>
   </div>
 </div>
