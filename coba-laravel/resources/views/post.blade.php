5b4595621d4ac9f4980296e6b9d2891cc88bddcc
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-12-01 15:42:30 +0700

    menyelesaikan video 21. Upload Image
    menyelesaikan video 21. Upload Image

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -7,6 +7,14 @@
   <p>By. <a href="posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
     {{ $post->category->name }}</a></p>
 
+    @if($post->image)
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid ">
+            </div>
+        @else
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid ">
+        @endif
+
   <!-- <h5>{{ $post["author"] }}</h5> -->
   {!! $post->body !!}
 
